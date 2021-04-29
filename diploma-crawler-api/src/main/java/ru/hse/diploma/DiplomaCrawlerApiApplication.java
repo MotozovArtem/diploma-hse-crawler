4906eecca44201917e4fcb1a812388bf007a06fe
Artem Motozov <motozov.a.v@gmail.com>
2021-04-29 23:25:21 +0300

    [-] Add API service

---
+++ diploma-crawler-api/src/main/java/ru/hse/diploma/DiplomaCrawlerApiApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaCrawlerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaCrawlerApiApplication.class, args);
+	}
+
+}
---
+++ diploma-crawler-api/src/test/java/ru/hse/diploma/DiplomaCrawlerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaCrawlerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
